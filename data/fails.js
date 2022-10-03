c6c2feebce025a40f62315bc2e23675c1a4c0708
Austin Flores <ctrl4ltdeleteme@gmail.com>
2022-10-03 17:37:25 +0200

    Added data for site

---
+++ data/fails.js
@@ -0,0 +1,29 @@
+const fails = {
+  // '2020-6': 3,
+  // '2020-7': 1,
+  // '2021-1': 1,
+  // '2021-3': 1,
+  // '2021-7': 1,
+  // '2021-9': 1,
+  // '2021-11': 0,
+  '2022-1': 0,
+  '2022-2': 1,
+  '2022-3': 1,
+  '2022-4': 1,
+  '2022-5': 7,
+  '2022-6': 0,
+  '2022-7': 4,
+  '2022-8': 0,
+  '2022-9': 1
+}
+/*
+Reverts:
+August 2022: c3ade19b09aea22eaccfd43d851a8803f9faaa25 - introduced flakey tests
+July 2022: 3582cab819d8ad2f127952f3e2a33a002274808d - Strapi key
+February 2022: 167d7a2f1ebd6053d28652d61507d336992701d4 - Deactivate production cdn
+October 2020: 77fac3d4819fd8120834904ce92cb61f48d2f2d5 - revert dev encryption key
+
+
+
+
+*/
