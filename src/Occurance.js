adcc5afba3f971794b44e0089dcadac46c8a5d48
Austin Flores <ctrl4ltdeleteme@gmail.com>
2022-10-07 19:05:25 +0200

    Allow for bucketing by day

--- src/Occurance.js
+++ src/Occurance.js
@@ -5,6 +5,13 @@ class Occurance {
     this.createdAt = params.createdAt;
   }
 
+  get dayBucket() {
+    const createdAt = new Date(this.createdAt);
+    const year = createdAt.getFullYear();
+    const month = createdAt.getUTCMonth() + 1;
+    const day = createdAt.getDate();
+    return `${year}-${month}-${day}`;
+  }
   // gets the bucket key for a given deploy week
   get weekBucket() {
     const createdAt = new Date(this.createdAt);
--- src/__test__/Occurance.spec.js
+++ src/__test__/Occurance.spec.js
@@ -7,8 +7,15 @@ beforeEach(() => {
   }
 })
 
+describe('#dayBucket', () => {
+  test('turns a createdAt into the format YYYY-M-D', () => {
+    const occurance = new Occurance(params)
+    expect(occurance.dayBucket).toBe('2022-8-9');
+  });
+});
+
 describe('#weekBucket', () => {
-  test('turns a created_at into the format YYYY-M-N', () => {
+  test('turns a createdAt into the format YYYY-M-N', () => {
     // N represents the week of the month
     const occurance = new Occurance(params)
     expect(occurance.weekBucket).toBe('2022-8-2');
@@ -24,7 +31,7 @@ describe('#weekBucket', () => {
 });
 
 describe('#monthBucket', () => {
-  test('turns a created_at into the format YYYY-M', () => {
+  test('turns a createdAt into the format YYYY-M', () => {
     // N represents the week of the month
     const occurance = new Occurance(params)
     expect(occurance.monthBucket).toBe('2022-8');
