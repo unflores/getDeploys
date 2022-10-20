88b9539f012dcbe50bad980350fee9cbf455f6af
Austin Flores <ctrl4ltdeleteme@gmail.com>
2022-10-20 17:45:46 +0200

    Add support for candidatesProcessor

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 require('dotenv/config');
 const syncSubjectWriter = require('./lib/syncSubjectWriter');
-const { createDeployGraphData } = require('./src/processors/getDeploys');
+const deployProcessor = require('./src/processors/getDeploys');
+const candidatesProcessor = require('./src/processors/getReleaseCandidates');
+
 const { DeployClient } = require('./src/DeployClient');
 
 new Promise(async (resolve) => {
   const authToken = process.env.TOKEN
   const repo = process.env.REPO
   const repoOwner = process.env.OWNER;
+  const absDirectory = process.env.PROJECT_DIRECTORY;
 
   if (process.env.npm_config_processor === 'getDeploys') {
     const deployClient = new DeployClient({ authToken, repo, repoOwner });
-    await createDeployGraphData(deployClient, syncSubjectWriter);
+    await deployProcessor.createDeployGraphData(deployClient, syncSubjectWriter);
+  } else if (process.env.npm_config_processor === 'getReleaseCandidates') {
+    await candidatesProcessor.createDeployGraphData(absDirectory, syncSubjectWriter)
   }
-
   resolve('done');
 }, (reason) => {
   console.log(reason);
