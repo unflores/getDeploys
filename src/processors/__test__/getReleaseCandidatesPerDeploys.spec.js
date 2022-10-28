792f5ac634ad5baf57ffd3c98fc5633b91e18c95
Austin Flores <ctrl4ltdeleteme@gmail.com>
2022-10-28 11:03:51 +0200

    Build ReleaseCadidatesPerDeploys

--- src/processors/__test__/getReleaseCandidatesPerDeploys.spec.js
+++ src/processors/__test__/getReleaseCandidatesPerDeploys.spec.js
@@ -1,12 +1,15 @@
-const { exportGraphData } = require('../getReleaseCandidatesPerDeploys');
+const { exportGraphData, releaseCandidatesPerDeploys } = require('../getReleaseCandidatesPerDeploys');
 const { DeployClient } = require('../../DeployClient');
 const gitLogReader = require('../../gitLogReader');
 const { Occurance } = require('../../Occurance');
 
 jest.mock('../../DeployClient');
-jest.mock('../../gitLogReader', () => {
-  return { getCommits: jest.fn().mockResolvedValue((_dir) => candidates()) }
+jest.doMock('../../gitLogReader', () => {
+  const candidates = new Occurance({ createdAt: '2022-08-10T09:32:18Z' });
+
+  return { getCommits: jest.fn().mockResolvedValue(candidates) }
 })
+
 
 DeployClient.mockImplementation(() => {
   return { getDeploys: async () => deploys() }
@@ -15,22 +18,64 @@ DeployClient.mockImplementation(() => {
 const deploys = () => {
   return new Occurance({ createdAt: '2022-08-09T09:32:18Z' });
 }
-const candidates = () => {
-  return new Occurance({ createdAt: '2022-08-10T09:32:18Z' });
-}
+
 
 const mockedWriter = { write: jest.fn() };
 
 describe('exportGraphData', () => {
-  it('writes to a file releaseCandidatesPerDeploys.js', () => {
-    exportGraphData('', new DeployClient(), mockedWriter);
+  it('writes to a file releaseCandidatesPerDeploys.js', async () => {
+    await exportGraphData('someDirectory', new DeployClient(), mockedWriter);
     writerParams = mockedWriter.write.mock.calls[0][0]
     expect(writerParams.subject).toBe('releaseCandidatesPerDeploys');
   });
 
-  it('writes proper structure', () => {
-    exportGraphData('', new DeployClient(), mockedWriter);
+  it('writes proper structure', async () => {
+    await exportGraphData('someDirectory', new DeployClient(), mockedWriter);
+    // console.log(mockedWriter.write.mock.calls[0])
     writerParams = mockedWriter.write.mock.calls[0][0]
     expect(Object.keys(writerParams.data)[0]).toBe('2022-8-1');
   });
+});
+
+describe.only('releaseCandidatesPerDeploys', () => {
+  it('returns the mean of commits per deploys for a month', () => {
+    expect(releaseCandidatesPerDeploys({
+      commits: [
+        new Occurance({ createdAt: '2022-07-29T09:32:18Z' }),
+        new Occurance({ createdAt: '2022-08-01T09:32:18Z' }),
+        new Occurance({ createdAt: '2022-08-01T09:32:18Z' }),
+      ],
+      deploys: [
+        new Occurance({ createdAt: '2022-08-05T09:32:18Z' })
+      ],
+      endDate: '2022-9'
+    })).toEqual([['2022-8', 2]])
+  });
+
+  describe('when multiple deploys', () => {
+    it('starts with the first', () => {
+      expect(releaseCandidatesPerDeploys({
+        commits: [
+          new Occurance({ createdAt: '2022-08-29T09:32:18Z' }),
+          new Occurance({ createdAt: '2022-07-29T09:32:18Z' })
+        ],
+        deploys: [
+          new Occurance({ createdAt: '2022-08-05T09:32:18Z' }),
+          new Occurance({ createdAt: '2022-07-05T09:32:18Z' })
+        ],
+        endDate: '2022-9'
+      })).toEqual([['2022-7', 1], ['2022-8', 1]])
+    })
+  });
+  describe('when NO deploys', () => {
+    it('returns empty Array', () => {
+      expect(releaseCandidatesPerDeploys({
+        commits: [
+          new Occurance({ createdAt: '2022-07-29T09:32:18Z' })
+        ],
+        deploys: [],
+        endDate: '2022-9'
+      })).toEqual([])
+    })
+  })
 });
--- src/processors/getReleaseCandidatesPerDeploys.js
+++ src/processors/getReleaseCandidatesPerDeploys.js
@@ -1,8 +1,41 @@
+const { build, next } = require('../../lib/dateLib');
+const moment = require('moment');
+const gitLogReader = require('../gitLogReader');
+
+function oldestDeploy(deploys) {
+  return deploys.reduce((a, b) => moment(a.createdAt) < moment(b.createdAt) ? a : b);
+}
+
+function commitsPerDeploysInMonth(commits, deploys, month) {
+  deploysPerMonth = deploys.filter((a) => a.monthBucket === month).length;
+  commitsPerMonth = commits.filter((a) => a.monthBucket === month).length;
+  return commitsPerMonth / deploysPerMonth;
+}
+
+function releaseCandidatesPerDeploys({ commits, deploys, endDate }) {
+  if (deploys.length === 0) {
+    return [];
+  }
+  let currentDate = oldestDeploy(deploys).monthBucket;
+  const candidatesPerDeploys = [];
+
+  while (build(currentDate) < build(endDate)) {
+    candidatesPerDeploys.push(
+      [currentDate, commitsPerDeploysInMonth(commits, deploys, currentDate)]
+    );
+    currentDate = next(currentDate, 'month');
+  }
+
+  return candidatesPerDeploys;
+}
+
 async function exportGraphData(absDirectory, deployClient, writer) {
-  const data = { '2022-8-1': 1 };
+  const data = releaseCandidatesPerDeploys({});
+  // const data = { '2022-8-1': 1 };
   writer.write({ subject: 'releaseCandidatesPerDeploys', data })
 }
 
 module.exports = {
-  exportGraphData
+  exportGraphData,
+  releaseCandidatesPerDeploys
 }
