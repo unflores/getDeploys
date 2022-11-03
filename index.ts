41869f7f98f1007d5019a13b22a710b6193eb3ed
Austin Flores <ctrl4ltdeleteme@gmail.com>
2022-11-03 22:58:06 +0100

    Setup development server

---
+++ index.ts
@@ -0,0 +1,9 @@
+// import { logger } from './config/logger'
+// import app from './src/server'
+// import { dbSetup } from './config/mongoose'
+
+// const port = process.env.PORT || '9090'
+// dbSetup()
+
+// app.listen(port)
+console.info(`hai`)
