a825ed09cd4173c539cb0136bdb313f267cb8d5d
Austin Flores <ctrl4ltdeleteme@gmail.com>
2022-12-22 21:19:06 +0100

    Typed occurances

--- tasks/Occurance.ts
+++ tasks/Occurance.ts
@@ -1,21 +1,22 @@
-const moment = require('moment');
+import * as moment from 'moment'
 // Something that happens at a given time
 class Occurance {
-  // @params params.createdAt 2022-08-09T09:32:18Z
-  constructor(params) {
+  createdAt: string
+
+  constructor(params: { createdAt: string }) {
     this.createdAt = params.createdAt;
   }
 
-  get dayBucket() {
+  get dayBucket(): string {
     return moment(this.createdAt).format('YYYY-M-D');
   }
 
   // gets the bucket key for a given deploy week
-  get monthBucket() {
+  get monthBucket(): string {
     return moment(this.createdAt).format('YYYY-M');
   }
 }
 
-module.exports = {
+export {
   Occurance
-};
+}
