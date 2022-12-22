6fb5d55a59bceeba5d79515ceff8fad768baf2d5
Austin Flores <ctrl4ltdeleteme@gmail.com>
2022-12-22 17:57:23 +0100

    Move tasks index to typescript

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,38 +1,53 @@
-require('dotenv/config');
-const syncSubjectWriter = require('../lib/syncSubjectWriter');
-const deployProcessor = require('./processors/getDeploys');
-const candidatesProcessor = require('./processors/getReleaseCandidates');
-const candidatesDeploysProcessor = require('./processors/getReleaseCandidatesPerDeploys');
+import * as dotenv from 'dotenv'
 
-const { DeployClient } = require('./DeployClient');
+dotenv.config()
+
+import * as syncSubjectWriter from '../lib/syncSubjectWriter'
+import * as deployProcessor from './processors/getDeploys'
+import * as candidatesProcessor from './processors/getReleaseCandidates'
+import * as candidatesDeploysProcessor from './processors/getReleaseCandidatesPerDeploys'
+
+import { DeployClient } from './DeployClient'
+
+function getProcessor(args: Array<string>): string {
+  const processorArg = args.find((arg) => arg.search('--processor=') > -1))
+  if (processorArg === undefined) {
+    return ''
+  }
+
+  return processorArg.split('--processor=')[1]
+}
 
 new Promise(async (resolve) => {
   const authToken = process.env.TOKEN
   const repo = process.env.REPO
-  const repoOwner = process.env.OWNER;
-  const absDirectory = process.env.PROJECT_DIRECTORY;
-  const deployClient = new DeployClient({ authToken, repo, repoOwner });
+  const repoOwner = process.env.OWNER
+  const absDirectory = process.env.PROJECT_DIRECTORY
+  const deployClient = new DeployClient({ authToken, repo, repoOwner })
+
+  const processor = getProcessor(process.argv)
 
   const processors = {
     getDeploys: 'getDeploys',
     getReleaseCandidates: 'getReleaseCandidates',
     getReleaseCandidatesPerDeploys: 'getReleaseCandidatesPerDeploys'
-  };
-
-
-  if (process.env.npm_config_processor === processors.getDeploys) {
-    await deployProcessor.createDeployGraphData(deployClient, syncSubjectWriter);
-  } else if (process.env.npm_config_processor === processors.getReleaseCandidates) {
-    await candidatesProcessor.createDeployGraphData(absDirectory, syncSubjectWriter)
-  } else if (process.env.npm_config_processor === processors.getReleaseCandidatesPerDeploys) {
-    await candidatesDeploysProcessor.exportGraphData(absDirectory, deployClient, syncSubjectWriter);
-  } else {
-    console.error(`FAILURE: Must provide a VALID processor. Please specifiy --processor=<${Object.keys(processors).join('|')}>`);
-    resolve('done');
-    return;
   }
 
-  resolve('done');
-}, (reason) => {
-  console.log(reason);
-});
+  if (processor === processors.getDeploys) {
+    await deployProcessor.createDeployGraphData(deployClient, syncSubjectWriter)
+  } else if (processor === processors.getReleaseCandidates) {
+    await candidatesProcessor.createDeployGraphData(absDirectory, syncSubjectWriter)
+  } else if (processor === processors.getReleaseCandidatesPerDeploys) {
+    await candidatesDeploysProcessor.exportGraphData(absDirectory, deployClient, syncSubjectWriter)
+  } else {
+    console.log(
+      `FAILURE: Must provide a VALID processor. ` +
+      `Please specifiy --processor=<${Object.keys(processors).join('|')}>`
+    )
+    resolve('done')
+    return
+  }
+
+  resolve('done')
+}
+).then(() => console.log('Done')).catch((reason) => console.log(reason))
