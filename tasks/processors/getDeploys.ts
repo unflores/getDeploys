7a1e6b9d641c369fbfdab58e8cabb7b4c205ed32
Austin Flores <ctrl4ltdeleteme@gmail.com>
2022-12-24 16:57:24 +0100

    OK lets go

--- tasks/processors/getDeploys.ts
+++ tasks/processors/getDeploys.ts
@@ -1,23 +1,37 @@
-function pagesToDeploysPerPeriod(deploys, period = 'month') {
-  const bucket = `${period}Bucket`;
+import { Occurance } from '../Occurance'
+
+type Client = {
+  getDeploys: () => Promise<Array<Occurance>>
+}
+
+type Data = {
+  [k: string]: number
+}
+
+type Writer = {
+  write: (params: { subject: string, data: Data }) => void
+}
+
+function pagesToDeploysPerPeriod(deploys: Array<Occurance>, period = 'month') {
+  const bucket = `${period}Bucket`
   return deploys.reduce((deploysPerPeriod, deploy) => {
     if (deploysPerPeriod[deploy[bucket]] === undefined) {
-      deploysPerPeriod[deploy[bucket]] = 0;
+      deploysPerPeriod[deploy[bucket]] = 0
     }
 
-    deploysPerPeriod[deploy[bucket]] += 1;
+    deploysPerPeriod[deploy[bucket]] += 1
 
-    return deploysPerPeriod;
-  }, {});
+    return deploysPerPeriod
+  }, {})
 }
 
-async function createDeployGraphData(deployClient, syncSubjectWriter) {
-  const deploys = await deployClient.getDeploys();
+async function createDeployGraphData(deployClient: Client, syncSubjectWriter: Writer) {
+  const deploys = await deployClient.getDeploys()
 
-  const deploysPerPeriod = pagesToDeploysPerPeriod(deploys);
-  syncSubjectWriter.write({ subject: 'deploys', data: deploysPerPeriod });
+  const deploysPerPeriod = pagesToDeploysPerPeriod(deploys)
+  syncSubjectWriter.write({ subject: 'deploys', data: deploysPerPeriod })
 }
 
-module.exports = {
+export {
   createDeployGraphData
 };
