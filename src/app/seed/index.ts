9d081ce5d456843110a0cb13fc33794de72df997
Austin Flores <ctrl4ltdeleteme@gmail.com>
2023-01-20 14:55:54 +0100

    Add dumb seed file

--- src/app/seed/index.ts
+++ src/app/seed/index.ts
@@ -9,13 +9,23 @@ if (process.env.NODE_ENV !== 'development') {
   process.exit()
 }
 
+const occurancesAtts = [
+  { bucket: '2022-10-11', createdAt: new Date(), type: 'deploy' },
+  { bucket: '2022-10-10', createdAt: new Date(), type: 'deploy' },
+  { bucket: '2022-10-10', createdAt: new Date(), type: 'deploy' },
+  { bucket: '2022-10-14', createdAt: new Date(), type: 'deploy' },
+  { bucket: '2022-10-01', createdAt: new Date(), type: 'contributer' },
+]
+
 new Promise(async (resolve, _reject) => {
   await connect(process.env.dbUrl)
-
+  console.log('Clear db')
   await Occurances.collection().deleteMany({})
-  await Occurances.insertOne({ bucket: '2022-10-10', createdAt: new Date(), type: 'deploy' })
 
+  console.log('Start inserting')
+  await Promise.all(occurancesAtts.map((atts) => Occurances.insertOne(atts)))
+
+  console.log(`Inserted ${occurancesAtts.length} occurances.`)
   await disconnect()
-  resolve('yass queen')
-}).then((results) => console.log(results))
-
+  resolve('Finished')
+}).then((results) => console.log(results)).catch((reason) => console.log({ reason }))
