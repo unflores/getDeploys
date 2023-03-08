08e9ce7383beb9b6db882145e5dd2896dc46cfd2
Austin Flores <ctrl4ltdeleteme@gmail.com>
2023-03-08 14:56:08 +0100

    Give syncSubjectWriter a setup to choose the write format

--- lib/syncSubjectWriter.ts
+++ lib/syncSubjectWriter.ts
@@ -1,21 +1,36 @@
 import { writeFileSync } from 'fs'
 
+interface WriteParams {
+  subject: string
+  data: { [k: string]: any }
+}
+
 interface Options {
   type?: 'js' | 'json'
 }
+class SyncSubjectWriter {
 
-function write(
-  { subject, data, opts = {} }: { subject: string, data: { [k: string]: any }, opts: Options }
-) {
+  options: Options
 
-  if (opts.type === 'js' || opts.type === undefined) {
-    writeFileSync(`./data/${subject}.js`, `const ${subject} = ${JSON.stringify(data)};`)
-  } else {
-    writeFileSync(`./data/${subject}.json`, JSON.stringify(data))
+  constructor(options: Options = {}) {
+    this.options = options
   }
 
+  write(
+    { subject, data }: WriteParams
+  ) {
+
+    if (this.options.type === 'js' || this.options.type === undefined) {
+      writeFileSync(
+        `./data/${subject}.js`, `const ${subject} = ${JSON.stringify(data)};`
+      )
+    } else {
+      writeFileSync(`./data/${subject}.json`, JSON.stringify(data))
+    }
+
+  }
 }
 
 export {
-  write
+  SyncSubjectWriter
 }
