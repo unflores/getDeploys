e149650c042995df1c16ab4f750be435236c96fd
Austin Flores <ctrl4ltdeleteme@gmail.com>
2023-11-09 09:36:49 +0100

    Clean up jest config

--- jest.config.js
+++ jest.config.js
@@ -11,11 +11,7 @@ module.exports = {
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        // ts-jest configuration goes here
-      },
-    ],
+    '^.+\\.tsx?$': 'ts-jest'
+
   },
 };
