c475dac5ee1700debd607f5fca85bafce897c145
Austin Flores <ctrl4ltdeleteme@gmail.com>
2024-03-11 17:18:45 +0100

    Move over another spec to ts

--- tasks/__test__/gitLogReader.spec.ts
+++ tasks/__test__/gitLogReader.spec.ts
@@ -1,27 +1,25 @@
-const child_process = require('child_process');
-const { GitLogReader } = require('../gitLogReader');
 
-jest.mock('child_process');
+import { jest, describe, test, expect} from '@jest/globals'
 
-const commits =
+const readCommits =
   `69adf19_commitsep_Thu Sep 29 14:36:26 2022 +0200
    67decd4_commitsep_Wed Sep 28 21:37:37 2022 +0200
    c1cfd3b_commitsep_Wed Sep 28 09:42:29 2022 +0200
    f2fc00c_commitsep_Wed Sep 28 09:11:10 2022 +0200
-  `;
-
-child_process.exec.mockImplementation(
-  (comamand, params, callback) => { callback(null, { stdout: commits, stderr: null }) }
-)
+  `
+jest.mock('../processors/utils', () => ({
+  asyncExec: jest.fn().mockReturnValue({ stdout: readCommits, stderr: null })
+}))
+import { GitLogReader } from '../gitLogReader'
 
 describe('gitLogReader', () => {
-  let reader = new GitLogReader('/some/directory')
+  const reader = new GitLogReader('/some/directory')
 
   describe('#commits', () => {
     test('properly splits commits', async () => {
       const commits = await reader.buildOccurances()
-      expect(commits.length).toBe(4);
-    });
+      expect(commits.length).toBe(4)
+    })
 
     test('extracts a date', async () => {
       const commits = await reader.buildOccurances()
@@ -31,7 +29,7 @@ describe('gitLogReader', () => {
         'Wed Sep 28 21:37:37 2022 +0200',
         'Wed Sep 28 09:42:29 2022 +0200',
         'Wed Sep 28 09:11:10 2022 +0200'
-      ]);
-    });
-  });
-});
+      ])
+    })
+  })
+})
